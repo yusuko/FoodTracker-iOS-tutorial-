b20b4eb9f24e9f21db5da3a07550eb859b356a35
Yusuke Anzai <qowgahoh@gmail.com>
2019-06-16 20:57:35 +0900

    Initial commit

---
+++ MyApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp2
+//
+//  Created by 安西佑介 on 2019/06/16.
+//  Copyright © 2019 安西佑介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
