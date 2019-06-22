0a9f01937d2a728b37ea7ac3cdc02add85a225db
Yusuke Anzai <qowgahoh@gmail.com>
2019-06-22 23:38:34 +0900

    Add custom class

---
+++ MyApp2/RatingControl.swift
@@ -0,0 +1,13 @@
+//
+//  RatingControl.swift
+//  MyApp2
+//
+//  Created by 安西佑介 on 2019/06/22.
+//  Copyright © 2019 安西佑介. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+}
