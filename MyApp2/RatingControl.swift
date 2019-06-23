f7242f0c934b1652f010d57e22f99186a964f897
Yusuke Anzai <qowgahoh@gmail.com>
2019-06-23 16:35:51 +0900

    Multiple 5 to buttons

--- MyApp2/RatingControl.swift
+++ MyApp2/RatingControl.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    //MARK: properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
     //MARK: Initialization
     
     override init(frame: CGRect) {
@@ -23,18 +28,23 @@ class RatingControl: UIStackView {
     
     //MARK: private func
     private func setupButtons() {
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        // Add constatins
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        //Add action
-        button.addTarget(self, action: #selector (ratingButtonTapped(button: )), for: .touchUpInside)
-        
-        // Add button to stack view
-        addArrangedSubview(button)
+        for _ in 0..<5 {
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            // Add constatins
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            //Add action
+            button.addTarget(self, action: #selector (ratingButtonTapped(button: )), for: .touchUpInside)
+            
+            // Add button to stack view
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
     
     //MARK: action
