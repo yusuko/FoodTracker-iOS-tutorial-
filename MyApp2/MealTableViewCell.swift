91e1db615d23f12c8d81b59d707dbc0a06fc7765
Yusuke Anzai <qowgahoh@gmail.com>
2019-06-27 22:03:20 +0900

    Add tableViewController and tableviewcell

---
+++ MyApp2/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  MyApp2
+//
+//  Created by 安西佑介 on 2019/06/27.
+//  Copyright © 2019 安西佑介. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
