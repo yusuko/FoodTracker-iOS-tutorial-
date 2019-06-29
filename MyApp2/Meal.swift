40310fbcd997ef52043c19b4aa792d36c24c2c5b
Yusuke Anzai <qowgahoh@gmail.com>
2019-06-29 17:51:42 +0900

    Add NSCoding to data persitent and set up url for preserving

--- MyApp2/Meal.swift
+++ MyApp2/Meal.swift
@@ -7,7 +7,40 @@
 //
 
 import UIKit
-class Meal {
+import os.log
+class Meal: NSObject, NSCoding {
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a MEal object", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        self.init(name: name, photo: photo, rating: rating)
+    }
+    
+    //MARK:: Types
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let  rating = "rating"
+    }
+    
+    //MARK:: Archiving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
     
     // MARK: properties
     
